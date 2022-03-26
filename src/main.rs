3e50298dd2fe2d28a4fe070e4276eaaac45ae007
cdsw <sushi90817564@gmail.com>
2022-03-26 13:24:48 +0800

    snapshot

--- src/main.rs
+++ src/main.rs
@@ -11,14 +11,29 @@ use druid::{
 };
 use car_simu_lib::{Map, SCALE};
 
-#[derive(Lens, Clone)]
-struct CarState {
+#[derive(Clone)]
+struct Car {
     inner: car_simu_lib::Car
 }
 
-impl Data for CarState {
+impl Deref for Car {
+    type Target = car_simu_lib::Car;
+
+    fn deref(&self) -> &Self::Target {
+        &self.inner
+    }
+}
+
+impl DerefMut for Car {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.inner
+    }
+}
+
+impl Data for Car {
     fn same(&self, other: &Self) -> bool {
-        self.lt == other.lt
+        self.lt.origin == other.lt.origin && self.lt.rotation_matrix == other.lt.rotation_matrix &&
+        self.rt.origin == other.rt.origin && self.rt.rotation_matrix == other.rt.rotation_matrix
     }
 }
 
@@ -30,10 +45,26 @@ impl Controller<Car, Painter<Car>> for CustomController {
             Event::KeyDown(ke) => {
                 match ke.key {
                     KbKey::ArrowUp => {
-                        car.borrow_mut().forward(0.3);
+                        // car.forward(0.3);
+                        ctx.request_anim_frame()
+                    }
+                    KbKey::ArrowDown => {
+                        car.forward(-0.3);
+                    }
+                    KbKey::ArrowLeft => {
+                        car.left_steer();
+                    }
+                    KbKey::ArrowRight => {
+                        car.right_steer();
                     }
                     _ => {}
                 }
+            }
+            Event::WindowConnected => {
+                ctx.request_focus();
+            }
+            Event::AnimFrame(x) => {
+                dbg!(x);
             }
             _ => {}
         }
@@ -44,18 +75,18 @@ fn main() {
     let window = WindowDesc::new(
         Painter::new(|ctx, car: &Car, env| {
             ctx.fill(Rect::from_center_size(
-                <(f64, f64)>::from(car.borrow().body.origin.to_real()),
-                (car.borrow().body.width*SCALE, car.borrow().body.height*SCALE)),
+                <(f64, f64)>::from(car.body.origin.to_real()),
+                (car.body.width*SCALE, car.body.height*SCALE)),
                 &Color::GREEN
             );
         })
         .controller(CustomController {})
     ).title("car-simu")
-        .window_size_policy(druid::WindowSizePolicy::Content)
-        .resizable(false);
+        .resizable(false)
+        .window_size((800., 800.));
     let mut map = car_simu_lib::ParallelParking::new();
     let mut car = map.car();
     AppLauncher::with_window(window)
-        .launch(Rc::new(RefCell::new(car)));
+        .launch(Car {inner: car});
 }
 
